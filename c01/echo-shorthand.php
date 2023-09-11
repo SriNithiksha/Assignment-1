90ef925f1b6eb88e90ca485e658570b2e00a16cf
SriNithiksha <122574570+SriNithiksha@users.noreply.github.com>
2023-09-11 10:45:05 -0800

    update echo-shorthand.php
    replace name with my name.
    add favorite candy name at the start of the array

--- c01/echo-shorthand.php
+++ c01/echo-shorthand.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Nithiksha';
+$favorites = ['lollipop', 'Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
